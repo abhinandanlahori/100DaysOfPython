f2e7bf1ea3404cf99c2110ae4e40e5ec987462bf
Abhinandan Lahori <abhinandanlahori@gmail.com>
2022-07-24 15:33:39 +0530

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,8 @@
+#Band Name Generator (day 1)
+
+print("Welcome to the band name generator.")
+print("What's the name of the city you grew up in?")
+city_name=input()
+print("what's your pet's name?")
+pet_name=input()
+print("Your band name could be " + city_name +  " " + pet_name )
